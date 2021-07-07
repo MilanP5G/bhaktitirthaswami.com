35f1619ef3533b36b6b99b24bd51bcb01d3c5f78
MilanP5G <milan_parmar@hotmail.co.uk>
2021-07-07 18:21:00 +0100

    imported logo for navbar and added under nav-bar div

--- bhaktitirthaswami.com/src/components/navbar/NavBar.js
+++ bhaktitirthaswami.com/src/components/navbar/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import btswamiLogo from '../../assets/BTSwamiLogo.png'
 import '../../styling/navbar.css';
 
 const NavBar = () => {
 
   return (
     <div className='nav-bar'>
+      <div className='nav-logo'>
+        <img alt='B.T.Swami' src={btswamiLogo} />
+      </div>
     </div>
   )
 
